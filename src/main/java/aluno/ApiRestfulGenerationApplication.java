39d98dc79e6fcde84745abeefaa09cfc762c31f0
Hiago Oliveira <hiagosantos223@gmail.com>
2023-10-23 23:51:20 -0300

    Initial commit

---
+++ src/main/java/aluno/ApiRestfulGenerationApplication.java
@@ -0,0 +1,13 @@
+package aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aluno/ApiRestfulGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
